1ea2ce6505d7fe360b5a5cb0d729f0d71793f4ab
Noah Franz <noahfranz13@gmail.com>
2024-07-19 11:27:10 -0700

    update sub repos

--- build.sh
+++ build.sh
@@ -3,14 +3,6 @@
 set -e
 
 if [[ -z "$(docker images -q astro-otter:latest 2> /dev/null)" ]]; then
-
-    # if the docker container exists and the repo has no local changes we can leave it
-    if [[ ! `git status --porcelain` ]]; then
-        echo "No local git changes so no need to rebuild the docker image!"
-        echo "Are you sure you didn't mean to just run `./rn`?"
-        exit 1
-    fi
-    
     # remove the docker container
     sudo docker image rm -f astro-otter
 fi
